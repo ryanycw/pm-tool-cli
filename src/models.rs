5bbb2f8beebf8f341ab1f19d0d366c3e7856e9ec
ryanycw <ryan@ryanycw.dev>
2025-06-09 15:04:57 +0800

    Add models for Epic, Story, and DBState with serialization support

---
+++ src/models.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct DBState {
+    // This struct represents the entire db state which includes the last_item_id, epics, and stories
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
