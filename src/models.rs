3ecc440504e2ccc1907939e6adfbfe9f1f815987
ryanycw <ryan@ryanycw.dev>
2025-06-11 18:21:01 +0800

    Add Action enum and implement Display trait for Status in models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Display};
 
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub enum Status {
@@ -8,6 +22,17 @@ pub enum Status {
     InProgress,
     Resolved,
     Closed,
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Status::Open => write!(f, "OPEN"),
+            Status::InProgress => write!(f, "IN PROGRESS"),
+            Status::Resolved => write!(f, "RESOLVED"),
+            Status::Closed => write!(f, "CLOSED"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
